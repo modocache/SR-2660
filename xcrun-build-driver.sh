73340b6ed946b0b7fa6f480dc38a8e5215b760cd
Brian Gesiak <modocache@gmail.com>
2017-10-19 02:31:36 -0700

    Works for me

--- xcrun-build-driver.sh
+++ xcrun-build-driver.sh
@@ -3,14 +3,14 @@ set -x
 
 SWIFTC="$HOME/local/Source/apple/build/Ninja-ReleaseAssert/swift-macosx-x86_64/bin/swiftc"
 SWIFT_SDK="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk"
-SWIFT_LIB="$HOME/local/Source/apple/build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/swift/macosx"
+SWIFT_LIB="$(xcode-select -p)/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx"
 CLANG="$HOME/local/Source/apple/build/Ninja-ReleaseAssert/llvm-macosx-x86_64/bin/clang"
 LLDB="$HOME/local/Source/apple/build/Ninja-ReleaseAssert/lldb-macosx-x86_64/CustomSwift-Release/lldb"
 
 mkdir -p out
 
 # Build Swift modules with debug info.
-$SWIFTC -c -g src/foo.swift \
+xcrun swiftc -c -g src/foo.swift \
   -target x86_64-apple-macosx10.12 \
   -sdk "$SWIFT_SDK" \
   -parse-as-library \
@@ -19,7 +19,7 @@ $SWIFTC -c -g src/foo.swift \
   -emit-objc-header-path out/Foo-Swift.h \
   -output-file-map output_map.json
 
-$SWIFTC -c -g src/bar.swift \
+xcrun swiftc -c -g src/bar.swift \
   -target x86_64-apple-macosx10.12 \
   -sdk "$SWIFT_SDK" \
   -parse-as-library \
@@ -29,13 +29,19 @@ $SWIFTC -c -g src/bar.swift \
   -output-file-map output_map.json
 
 # Build the main program, without using dSYM.
-$SWIFTC src/main.swift -o out/driver-main \
-  out/foo.o out/bar.o out/Foo.swiftmodule out/Bar.swiftmodule \
+xcrun swiftc src/main.swift -o out/driver-main \
+  out/foo.o out/bar.o \
   -target x86_64-apple-macosx10.12 \
   -sdk "$SWIFT_SDK" \
   -Iout \
-  -L"$SWIFT_LIB"
+  -L"$SWIFT_LIB" \
+
+# Build the main program using -g, which results in a different error.
+#$SWIFTC -g src/main.swift -o out/driver-main out/Foo.swiftmodule out/Bar.swiftmodule \
+#  -sdk "$SWIFT_SDK" \
+#  -Iout \
+#  -L"$SWIFT_LIB" \
+#  -Xlinker -rpath -Xlinker "$SWIFT_LIB"
 
 # Run LLDB commands.
-$LLDB out/driver-main -b -s lldb_commands.txt
 
